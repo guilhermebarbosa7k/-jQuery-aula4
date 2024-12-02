e2bb4cdc17e8ead750dd91d473c8fe89c27f751e
guilhermebarbosa7k <llucasguilherme16@gmail.com>
2024-12-02 17:35:22 -0300

    commit "projeto completo"

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ $(document).ready(function(){
     $('#botao-cancelar').click(function(){
         $('form').slideUp()
     })
-    $('form').on('sumbit',function(e){
+    $('form').on('submit',function(e){
         e.preventDefault()
-        const enderecoDaNovaImagem=$('#endereco-imagem-nova').val
+        const enderecoDaNovaImagem=$('#endereco-imagem-nova').val()
         const novoItem=$('<li style="display: none"></li>')
         $(`<img src="${enderecoDaNovaImagem}"/>`).appendTo(novoItem)
         $(`
@@ -18,7 +18,7 @@ $(document).ready(function(){
         </div>
     `).appendTo(novoItem)
     $(novoItem).appendTo('ul')
-    $(novoItem).fadeIn()
+    $(novoItem).fadeIn(1000)
     $('#endereco-imagem-nova').val('')
     })
 })
